a25c13ebbbaf51eba8141096f675895a25236581
Adam Groszer <agroszer@mail.com>
2010-01-27 12:01:13 +0000

    back to dev

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup (
     name='z3c.jsonrpcproxy',
-    version='0.6.0',
+    version='0.7.0dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "JSON RPC (javascript) proxy implementation for Zope3",
